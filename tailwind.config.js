3cc877df12b4bbda59d9cef1ad221c3e0de2aa8e
Tim <timtb.dev@gmail.com>
2023-03-30 12:29:06 +0800

    fix: fixed dependencies issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,39 +28,11 @@ module.exports = {
         "t-2xl": "0 -25px 50px -12px rgba(0, 0, 0, 0.25)",
         "t-3xl": "0 -35px 60px -15px rgba(0, 0, 0, 0.3)",
       },
-      colors: {
-        // ...
-        primaryBackground: {
-          DEFAULT: "rgb(255,255,255)",
-          dark: "rgb(22,22,22)",
-        },
-        primaryTitle: {
-          DEFAULT: "rgb(34, 34, 34)",
-          dark: "#a3a3a3",
-        },
-        primaryBorder: {
-          DEFAULT: "rgb(221, 221, 221)",
-          dark: "rgb(64 64 64 / 0.4)",
-        },
-        primaryShadow: {
-          DEFAULT: "rgb(31 41 55 / 0.2)",
-          dark: "rgb(38 38 38 / 0.2)",
-        },
-        menuHoverBg: {
-          DEFAULT: "rgb(17 24 39 / 0.05)",
-          dark: "rgb(38 38 38 / 0.5)",
-        },
-        menuHoverRing: {
-          DEFAULT: "rgb(17 24 39 / 0.2)",
-          dark: "rgb(163 163 163 / 0.2)",
-        },
-      },
     },
   },
   plugins: [
     require("@tailwindcss/typography"),
     require("@tailwindcss/forms"),
-    require("@tailwindcss/line-clamp"),
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/container-queries"),
   ],
