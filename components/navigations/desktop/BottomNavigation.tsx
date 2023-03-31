5dc78e5895cabd576a24f00378be74e2312cfecc
Tim <timtb.dev@gmail.com>
2023-03-31 09:31:43 +0800

    fix: removed animation from footer and fixed related animation's bugs

--- components/navigations/desktop/BottomNavigation.tsx
+++ components/navigations/desktop/BottomNavigation.tsx
@@ -12,7 +12,7 @@ const BottomNavigation: FC<BottomNavigationProps> = ({ footer }) => {
   return (
     <>
       <div className="fixed bottom-0 left-0 hidden w-full border-t-[1.5px] border-slate-300/50 bg-gray-50 shadow-t-sm dark:border-slate-600/50 dark:bg-slate-800 dark:shadow-slate-900 md:flex">
-        <div className="mx-auto w-full max-w-7xl px-6 py-3  md:flex md:items-center md:justify-between lg:px-8">
+        <div className="mx-auto w-full max-w-7xl px-4 py-3 md:flex md:items-center md:justify-between">
           <div className="flex justify-center space-x-6 md:order-2">
             {footer.socials.map((item, idx) => (
               <a
--- components/navigations/mobile/MobileBottomNavigation.tsx
+++ components/navigations/mobile/MobileBottomNavigation.tsx
@@ -1,54 +1,15 @@
-"use client"
-
-import { FC, useEffect, useState } from "react"
-import { motion } from "framer-motion"
-import { useMediaQuery } from "react-responsive"
+import { FC } from "react"
 
 import { Menu } from "types"
-import { useScrollListener } from "@hooks/useScrollListener"
 
 interface MobileBottomNavigationProps {
   menu: Menu[]
 }
 
-const variants = {
-  visible: { opacity: 1, y: 0 },
-  initial: { opacity: 0, y: 0 },
-  hidden: { opacity: 0, y: 50 },
-}
-
 const MobileBottomNavigation: FC<MobileBottomNavigationProps> = ({ menu }) => {
-  const isMobile = useMediaQuery({ query: "(max-width: 768px)" })
-  const [showNavBar, setShowNavBar] = useState(false)
-  const scroll = useScrollListener()
-
-  useEffect(() => {
-    if (
-      scroll.y > 150 &&
-      scroll.y - scroll.lastY > 0 &&
-      !showNavBar &&
-      isMobile
-    ) {
-      setShowNavBar(true)
-    } else if (
-      scroll.y < 150 &&
-      scroll.y - scroll.lastY < 0 &&
-      showNavBar &&
-      isMobile
-    ) {
-      setShowNavBar(false)
-    }
-  }, [scroll.y, scroll.lastY, showNavBar, isMobile])
-
   return (
     <>
-      <motion.div
-        className="sticky bottom-0 left-0 z-50 h-16 w-full border-t border-gray-200 bg-white dark:border-gray-600 dark:bg-gray-700 md:hidden"
-        initial="initial"
-        animate={showNavBar ? "visible" : "hidden"}
-        variants={variants}
-        transition={{ ease: [0.1, 0.25, 0.3, 1], duration: 0.6 }}
-      >
+      <div className="sticky bottom-0 left-0 z-50 h-16 w-full border-t border-gray-200 bg-white dark:border-gray-600 dark:bg-gray-700 md:hidden">
         <div className="mx-auto grid h-full max-w-lg grid-cols-4 font-medium">
           {menu.slice(0, 4).map((item) => (
             <button
@@ -66,7 +27,7 @@ const MobileBottomNavigation: FC<MobileBottomNavigationProps> = ({ menu }) => {
             </button>
           ))}
         </div>
-      </motion.div>
+      </div>
     </>
   )
 }
