dced491579c79202806aec4304799fbd9b289b8b
Tim <timtb.dev@gmail.com>
2023-03-31 08:43:48 +0800

    fix: hopefully finally fixed footer issue

--- components/navigations/desktop/BottomNavigation.tsx
+++ components/navigations/desktop/BottomNavigation.tsx
@@ -1,5 +1,6 @@
-import { FC, useState } from "react"
+import { FC, useEffect, useState } from "react"
 import { Transition } from "@headlessui/react"
+import { useMediaQuery } from "react-responsive"
 
 import { Footer } from "types"
 
@@ -7,12 +8,12 @@ interface BottomNavigationProps {
   footer: Footer
 }
 const BottomNavigation: FC<BottomNavigationProps> = ({ footer }) => {
-  const [isShowing, setIsShowing] = useState(false)
+  const isTablet = useMediaQuery({ query: "(min-width: 640px)" })
   return (
     <>
-      <div className="sticky bottom-0 left-0 border-t-[1.5px] border-slate-300/50 bg-gray-50 shadow-t-sm dark:border-slate-600/50 dark:bg-slate-800 dark:shadow-slate-900">
-        <div className="mx-auto flex max-w-7xl items-center justify-between px-6 py-3 lg:px-8">
-          <div className="order-2 flex justify-center space-x-6">
+      <div className="fixed bottom-0 left-0 hidden w-full border-t-[1.5px] border-slate-300/50 bg-gray-50 shadow-t-sm dark:border-slate-600/50 dark:bg-slate-800 dark:shadow-slate-900 md:flex">
+        <div className="mx-auto w-full max-w-7xl px-6 py-3  md:flex md:items-center md:justify-between lg:px-8">
+          <div className="flex justify-center space-x-6 md:order-2">
             {footer.socials.map((item, idx) => (
               <a
                 key={idx + item.name}
@@ -24,7 +25,7 @@ const BottomNavigation: FC<BottomNavigationProps> = ({ footer }) => {
               </a>
             ))}
           </div>
-          <div className="order-1 mt-0">
+          <div className="mt-4 md:order-1 md:mt-0">
             <p className="text-center text-sm text-slate-500 dark:text-slate-400">
               &copy; {footer.copyright}
             </p>
