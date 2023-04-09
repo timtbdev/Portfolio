0d2ac9a82b89008f7776023a6b0c1671c5389e47
Tim <timtb.dev@gmail.com>
2023-04-10 07:54:20 +0800

    absolute inset-0

--- app/layout.tsx
+++ app/layout.tsx
@@ -82,7 +82,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
     >
       <body
         className={cn(
-          "bg-white font-sans antialiased dark:bg-slate-800/90"
+          "absolute inset-0 bg-white font-sans antialiased dark:bg-slate-800/90"
         )}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
