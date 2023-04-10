5652b10caa82d1a468d46eebe38ce8c4951f14c6
Tim <timtb.dev@gmail.com>
2023-04-10 19:23:32 +0800

    added header and footer animation

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,14 +4,40 @@ import { usePathname } from "next/navigation"
 
 import { footer, menu } from "../config"
 import { BottomNavigation, MobileBottomNavigation } from "./navigations"
+import { Fragment, useEffect, useState } from "react"
+import useBoundedScroll from "@/hooks/useBoundedScroll"
+import { motion, useTransform } from "framer-motion"
 
+const variants = {
+  visible: { opacity: 1, y: 0 },
+  initial: { opacity: 0, y: 75 },
+  hidden: { opacity: 0, y: 25 },
+}
 
 const Footer = () => {
   const currentPath = usePathname()
+  let { scrollYBoundedProgress } = useBoundedScroll(300)
+  let scrollYBoundedProgressThrottled = useTransform(
+    scrollYBoundedProgress,
+    [0, 0.75, 1],
+    [0, 0, 1]
+  )
+  const [hidden, setHidden] = useState(false)
+
+  useEffect(() => {
+    return scrollYBoundedProgressThrottled.onChange((current) => {
+      current == 1 ? setHidden(true) : setHidden(false)
+    })
+  }, [scrollYBoundedProgressThrottled, hidden])
   return (
     <>
-      <footer
+      <motion.footer
         className="sticky bottom-0 z-40 border-y-[1.2px] border-slate-300 bg-gray-50 shadow-t-sm dark:border-slate-600/50 dark:bg-slate-800 dark:shadow-slate-900"
+        initial="hidden"
+        animate={hidden ? "visible" : "hidden"}
+        variants={variants}
+        onHoverStart={() => setHidden(false)}
+        transition={{ ease: [0.1, 0.25, 0.3, 1], duration: 0.6 }}
       >
         <nav
           className="mx-auto hidden max-w-5xl items-center justify-between md:flex"
@@ -25,7 +51,7 @@ const Footer = () => {
         >
           <MobileBottomNavigation menu={menu} path={currentPath} />
         </nav>
-      </footer>
+      </motion.footer>
     </>
   )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,10 @@
 "use client"
 
-import { Fragment, useEffect, useRef, useState } from "react"
+import { Fragment, useEffect, useState } from "react"
 import { usePathname } from "next/navigation"
-import useScroll from "@/hooks/useScroll"
+import useBoundedScroll from "@/hooks/useBoundedScroll"
 import { Disclosure } from "@headlessui/react"
-import { motion } from "framer-motion"
+import { motion, useTransform } from "framer-motion"
 
 import { menu, profile } from "@config/index"
 import {
@@ -15,79 +15,70 @@ import {
   Switch,
 } from "./navigations"
 
+const variants = {
+  visible: { opacity: 1, y: 0 },
+  initial: { opacity: 0, y: -75 },
+  hidden: { opacity: 0, y: -25 },
+}
+
 const Header = () => {
   const currentPath = usePathname()
-  const [hideNavBar, setHideNavBar] = useState(false)
-  const scroll = useScroll()
-
-  const scrolled50Ref = useRef(false)
-
-  useEffect(() => {
-    const scrollFun = () => {
-      if (window.pageYOffset > 50) {
-        if (!scrolled50Ref.current) {
-          scrolled50Ref.current = true
-          console.log("scrolled 50 px")
-        }
-      } else {
-        scrolled50Ref.current = false
-      }
-
-      if (window.pageYOffset % 50 >= 45) {
-        console.log("scrolled 50-ish pixels")
-      }
-    }
-
-    window.addEventListener("scroll", scrollFun)
-
-    return () => {
-      window.removeEventListener("scroll", scrollFun)
-    }
-  }, [])
+  let { scrollYBoundedProgress } = useBoundedScroll(200)
+  let scrollYBoundedProgressThrottled = useTransform(
+    scrollYBoundedProgress,
+    [0, 0.75, 1],
+    [0, 0, 1]
+  )
+  const [hidden, setHidden] = useState(false)
 
   useEffect(() => {
-    console.log(scroll.y)
-    if (scroll.y > 150 && scroll.y - scroll.lastY > 0) {
-      setHideNavBar(true)
-    } else if (scroll.y < 150 && scroll.y - scroll.lastY < 0) {
-      setHideNavBar(false)
-    }
-  }, [scroll.y, scroll.lastY, hideNavBar])
+    return scrollYBoundedProgressThrottled.onChange((current) => {
+      current == 1 ? setHidden(true) : setHidden(false)
+    })
+  }, [scrollYBoundedProgressThrottled, hidden])
 
   return (
-    <Disclosure
-      as="nav"
+    <motion.div
       className="border-y-1 sticky top-0 z-50 border-black/5 bg-gray-50/50 shadow-sm shadow-gray-300 backdrop-blur dark:border-white/10 dark:bg-slate-800 dark:shadow-slate-900/60"
+      animate={hidden ? "hidden" : "visible"}
+      variants={variants}
+      onHoverStart={() => setHidden(false)}
+      transition={{ ease: [0.1, 0.25, 0.3, 1], duration: 0.6 }}
     >
-      {({ open }) => (
-        <>
-          <motion.nav
-            className="mx-auto flex max-w-5xl items-center justify-between px-2 py-4"
-            aria-label="Global"
-          >
-            {/* Logo */}
-            <div className="flex flex-1 justify-start pl-2">
-              <Logo image={profile.image} description={profile.description} />
-            </div>
+      <Disclosure>
+        {({ open }) => (
+          <>
+            <nav
+              className="mx-auto flex max-w-5xl items-center justify-between px-2 py-4"
+              aria-label="Global"
+            >
+              {/* Logo */}
+              <div className="flex flex-1 justify-start pl-2">
+                <Logo image={profile.image} description={profile.description} />
+              </div>
 
-            {/* Navigation */}
-            <div className="hidden gap-x-6 sm:flex sm:flex-1">
-              <Navigation path={currentPath} menu={menu} />
-            </div>
-            {/* Dark Mode */}
-            <div className="hidden justify-end md:flex md:flex-1">
-              <Switch />
-            </div>
-            {/* Mobile Menu */}
-            <div className="flex flex-1 justify-end pr-2 md:hidden">
-              <MobileMenuButton open={open} />
-            </div>
-          </motion.nav>
-          {/* <!-- Mobile Navigation --> */}
-          <MobileNavigation fragment={Fragment} menu={menu} />
-        </>
-      )}
-    </Disclosure>
+              {/* Navigation */}
+              <div>
+                <div className="z-10 hidden gap-x-6 sm:flex sm:flex-1">
+                  <Navigation path={currentPath} menu={menu} />
+                </div>
+              </div>
+
+              {/* Dark Mode */}
+              <div className="hidden justify-end md:flex md:flex-1">
+                <Switch />
+              </div>
+              {/* Mobile Menu */}
+              <div className="flex flex-1 justify-end pr-2 md:hidden">
+                <MobileMenuButton open={open} />
+              </div>
+            </nav>
+            {/* <!-- Mobile Navigation --> */}
+            <MobileNavigation fragment={Fragment} menu={menu} />
+          </>
+        )}
+      </Disclosure>
+    </motion.div>
   )
 }
 
