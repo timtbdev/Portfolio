dc6b30f05d862c3ad96e2759e8ea58416707cecc
Tim <timtb.dev@gmail.com>
2023-04-15 06:45:15 +0800

    fix: bla bla bla

--- app/icon.tsx
+++ app/icon.tsx
@@ -13,7 +13,7 @@ export default function icon() {
       <div
         style={{
           fontSize: 24,
-          backgroundColor: "black",
+          backgroundColor: "transparent",
           width: "100%",
           height: "100%",
           display: "flex",
@@ -24,36 +24,39 @@ export default function icon() {
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="30"
-          height="30"
-          viewBox="0 0 30 30"
+          width="42"
+          height="42"
+          viewBox="0 0 42 42"
         >
-          <g
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            fill="none"
-            stroke="#F7F7F7"
-          >
+          <g>
             <path
-              d="M25.53,27.583 c-0.185-3.257-2.042-4.29-4.758-5.195c-1.942-0.647-2.567-2.609-2.768-3.806"
-              stroke="#F7F7F7"
-            ></path>{" "}
+              fill="#d5d6d6"
+              d="M21.02 9.31a11.71 11.71 0 1 0 0 23.42 11.71 11.71 0 1 0 0-23.42z"
+            ></path>
             <path
-              d="M13.993,18.581 c-0.201,1.197-0.824,3.16-2.765,3.808c-2.716,0.905-4.59,1.925-4.774,5.182"
-              stroke="#F7F7F7"
-            ></path>{" "}
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              fill="#d1b38e"
+              d="M18.47 20.89h5.1v5.81h-5.1z"
+            ></path>
             <path
-              d="M16,19L16,19 c-2.761,0-5-2.239-5-5v-2c0-2.761,2.239-5,5-5h0c2.761,0,5,2.239,5,5v2C21,16.761,18.761,19,16,19z"
-              stroke="#F7F7F7"
-            ></path>{" "}
-            <ellipse
-              transform="matrix(0.0474 -0.9989 0.9989 0.0474 -0.7397 31.2244)"
-              cx="16"
-              cy="16"
-              rx="15"
-              ry="15"
-            ></ellipse>
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              fill="#dabfa9"
+              d="M21.02 23.57a4.07 4.07 0 0 1-4.07-4.08v-1.52a4.07 4.07 0 1 1 8.14 0v1.52a4.07 4.07 0 0 1-4.07 4.08z"
+            ></path>
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              fill="#3b82f6"
+              d="M21.02 32.73c3.43 0 6.51-1.47 8.66-3.82 0-0.84-0.62-1.81-1.39-2.16l-4.72-2.16s-1.02 1.02-2.55 1.01-2.55-1.02-2.55-1.01l-4.72 2.16c-0.77 0.35-1.38 1.31-1.39 2.16a11.69 11.69 0 0 0 8.66 3.82z"
+            ></path>
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              fill="#000000"
+              d="M23.06 15.93a2.04 2.04 0 0 1 2.03 2.04v-2.55c0-1.12-0.91-2.04-2.03-2.04h-0.25l-0.54-1.07a0.55 0.55 0 0 0-0.7-0.26l-3.2 1.27c-0.78 0.31-1.42 1.25-1.42 2.1v2.55a2.04 2.04 0 0 1 2.03-2.04h4.08z"
+            ></path>
           </g>
         </svg>
       </div>
