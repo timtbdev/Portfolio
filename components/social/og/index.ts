cd0f1f9dba8d260be9129cd357ae6990eb027938
Tim <timtb.dev@gmail.com>
2023-04-17 08:47:22 +0800

    added og image creator

--- components/social/og/index.ts
+++ components/social/og/index.ts
@@ -1,3 +1,3 @@
 export { default as Page } from "./Page"
 export { default as Post } from "./Post"
-export { default as ScreenShot } from "./Screenshot"
+export { default as ScreenShot } from "./ScreenShot"
