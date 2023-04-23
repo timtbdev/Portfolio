738488f29ec7a9ee15bb8a3c2f8ea6e41fabeb2f
Tim <timtb.dev@gmail.com>
2023-04-23 17:49:30 +0800

    blabla

--- components/ogImage/Wrappter.tsx
+++ components/ogImage/Wrappter.tsx
@@ -8,14 +8,7 @@ interface WrapperProps {
 }
 
 export const Wrapper = ({ children }: WrapperProps) => (
-  <div tw="flex w-full h-full">
-    <img
-      tw="flex absolute left-0 top-0 w-full h-[110%]"
-      src="https://timtb.dev/static/social-bg-dark-lines.jpg"
-      alt="background"
-      width="1200"
-      height="600"
-    />
+  <div tw="flex w-full h-full bg-gray-100">
     <div tw="flex flex-col w-full h-full px-[80px] py-[70px] items-start justify-center">
       {children}
     </div>
